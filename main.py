3c8eab7eceff96a29bca0b756087d66555162a83
vphan404 <vphan404@users.noreply.github.com>
2020-08-12 09:21:46 -0400

    Outlined program

---
+++ main.py
@@ -0,0 +1,12 @@
+# Web scrape songs from Pandora with liked or not liked attribute
+
+# Create dataframe of Pandora songs with Spotify audio features
+
+# Create model based on features
+
+# Adjust models via balancing and different estimators
+
+# Find new recommendations
+## Randomly select songs from the Spotify database
+## Use graph network to find songs of similiar users
+## Use songs similiar in features
