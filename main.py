8dcf885350ee9f61ac6de0f93de2907047b3170e
vphan404 <vphan404@users.noreply.github.com>
2020-08-15 11:13:48 -0400

    Separated outlined functions to their own modules

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
 # Standard library
-import json
 import time
+
 # Related third party
 from bs4 import BeautifulSoup # html/xml parser
 import pandas as pd # dataframe
-import requests # grab webpages
-from selenium import webdriver # affect javascript actions easily
-from selenium.webdriver.common.keys import Keys # common key commands
+import requests # for making HTTP requests
 
-# Load json credentials
-with open("config.json",'r') as f:
-  config = json.load(f)
+# Local application
+import webscraper
+from webscraper import WebScraper
 
-# Web scrape songs from Pandora with liked or not liked attribute
-DRIVER_PATH = "assets\\chromedriver.exe"
-driver = webdriver.Chrome(DRIVER_PATH)
-driver.implicitly_wait()
+### Web scrape songs from Pandora with liked or not liked attribute
 
-driver.get("https://www.pandora.com/account/sign-in")
-driver.find_element_by_name('username').send_keys(config['pandora']['username'])
-driver.find_element_by_name('password').send_keys(config['pandora']['password'])
-driver.find_element_by_css_selector("button[data-qa='login_button']").click()
+### Create dataframe of Pandora songs with Spotify audio features
 
+### Create model based on features
 
-# Create dataframe of Pandora songs with Spotify audio features
+### Adjust models via balancing and different estimators
 
-# Create model based on features
-
-# Adjust models via balancing and different estimators
-
-# Find new recommendations
+### Find new recommendations
 ## Randomly select songs from the Spotify database
 ## Use graph network to find songs of similiar users
 ## Use songs similiar in features
+
+def main():
+  webscraper = WebScraper(is_headless=False)
+  webscraper.login()
+  time.sleep(6)
+  webscraper.get_stations()
+  input('Press ENTER to exit browser')
+  webscraper.logout()
+  time.sleep(2)
+  webscraper.close_browser()
+  input('Press ENTER to exit program')
+
+if __name__ == "__main__":
+    main()
