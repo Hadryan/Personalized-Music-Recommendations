260781ba8a9bbbd923e3b391e747932dd9729eed
vphan404 <vphan404@users.noreply.github.com>
2020-08-16 02:20:10 -0400

    get_stations is complete and returns a list of all stations on the my collections page

--- webscraper.py
+++ webscraper.py
@@ -14,14 +14,15 @@ class WebScraper:
     def __init__(self, is_headless=True, config_file="config.json"):
         OPTIONS = Options()
         OPTIONS.headless = is_headless
-        OPTIONS.add_argument("--window-size=1360,960")
+        OPTIONS.add_argument("start-maximized")
 
         with open(config_file,'r') as f:
             self.config = json.load(f)
 
         DRIVER_PATH = self.config['assets']['driver']
         self.driver = webdriver.Chrome(DRIVER_PATH, options=OPTIONS)
-        self.driver.implicitly_wait(10)    
+        
+        self.driver.implicitly_wait(10)
     
     def login(self, credentials_file="credentials.json"):
         # Load json credentials
@@ -33,7 +34,13 @@ class WebScraper:
         self.driver.find_element_by_name('username').send_keys(credentials['pandora']['username'])
         self.driver.find_element_by_name('password').send_keys(credentials['pandora']['password'])
         self.driver.find_element_by_css_selector("button[data-qa='login_button']").click()
-    
+        
+        # try:
+        # Using implciit wait and explicit wait together causes errors
+        #     WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "nowPlayingTopInfo__artContainer__art")))
+        # except:
+        #     self.close_browser()
+
     def logout(self):
         pandora_logout_url = self.config['URLs']['pandora']['logout']
         self.driver.get(pandora_logout_url)
@@ -42,12 +49,20 @@ class WebScraper:
         self.driver.quit()
     
     def get_stations(self):
-        collections_url = self.config['URLs']['pandora']['collections']
-        self.driver.get(collections_url)
-        stations = self.driver.find_elements(By.CLASS_NAME, 'InfiniteGrid__contents__itemContainer')
-        for station in stations:
-            print(station)
-    
+        # Click My Collections
+        self.driver.find_element(By.CSS_SELECTOR, "a[data-qa='header_my_stations_link']").click()
+
+        station_elements = self.driver.find_elements(By.CLASS_NAME, 'InfiniteGrid__contents__itemContainer')
+        
+        stations = [self.driver.find_element(By.CLASS_NAME, "GridItem__caption__link").get_attribute('href') for element in station_elements]
+
+        # for element in station_elements:
+        #     # stations += adds the letters instead of full string
+        #     stations.append(self.driver.find_element(By.CLASS_NAME, "GridItem__caption__link").get_attribute('href'))
+        
+        return stations
+    def scroll_to_bottom(self):
+        
     def go_to_URL(self, URL):
         self.driver.get(URL)
 
