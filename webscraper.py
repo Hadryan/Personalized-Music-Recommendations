5fdba820725528aae0e6692fb521a1bb0231810c
vphan404 <vphan404@users.noreply.github.com>
2020-09-02 12:22:35 -0400

    Finished get_songs and auxiliary functions along with prettified code

--- webscraper.py
+++ webscraper.py
@@ -2,41 +2,59 @@
 import json
 import time
 import random
+import sys
 
 # Related third party
-from selenium import webdriver # Launch/initialise a browser and affect javascript easily
-from selenium.webdriver.chrome.options import Options # Options for intialzing browser
-from selenium.webdriver.common.by import By # Search for things using specific parameters
-from selenium.webdriver.common.keys import Keys # Send key commands easily
-from selenium.webdriver.support.ui import WebDriverWait # Wait for a page to load
-from selenium.webdriver.support import expected_conditions as EC # Specify what you are looking for on a specific page in order to determine that the webpage has loaded
-from selenium.common.exceptions import TimeoutException # Handling a timeout situation
+# Launch/initialise a browser and affect javascript easily
+from selenium import webdriver
+# Options for intialzing browser
+from selenium.webdriver.chrome.options import Options
+# Search for things using specific parameters
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys  # Send key commands easily
+from selenium.webdriver.support.ui import WebDriverWait  # Wait for a page to load
+# Specify what you are looking for on a specific page in order to determine that the webpage has loaded
+from selenium.webdriver.support import expected_conditions as EC
+# Handling a timeout situation
+from selenium.common.exceptions import TimeoutException
+
 
 class WebScraper:
     def __init__(self, is_headless=True, config_file="config.json"):
         OPTIONS = Options()
         OPTIONS.headless = is_headless
-        OPTIONS.add_argument("start-maximized")
+        OPTIONS.add_argument("--mute-audio")
 
-        with open(config_file,'r') as f:
+        if is_headless:
+            user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+            OPTIONS.add_argument(f"user-{user_agent}")
+            OPTIONS.add_argument("window-size=2560x1440")
+            OPTIONS.add_argument("--log-level=3")
+        else:
+            OPTIONS.add_argument("--start-maximized")
+
+        with open(config_file, 'r') as f:
             self.config = json.load(f)
 
         DRIVER_PATH = self.config['assets']['driver']
         self.driver = webdriver.Chrome(DRIVER_PATH, options=OPTIONS)
-        
-        self.driver.implicitly_wait(20)
-    
+
+        self.driver.implicitly_wait(10)
+
     def login(self, credentials_file="credentials.json"):
         # Load json credentials
-        with open(credentials_file,'r') as f:
+        with open(credentials_file, 'r') as f:
             credentials = json.load(f)
 
         pandora_login_url = self.config['URLs']['pandora']['login']
         self.driver.get(pandora_login_url)
-        self.driver.find_element_by_name("username").send_keys(credentials['pandora']['username'])
-        self.driver.find_element_by_name("password").send_keys(credentials['pandora']['password'])
-        self.driver.find_element_by_css_selector("button[data-qa='login_button']").click()
-        
+        self.driver.find_element_by_name("username").send_keys(
+            credentials['pandora']['username'])
+        self.driver.find_element_by_name("password").send_keys(
+            credentials['pandora']['password'])
+        self.driver.find_element_by_css_selector(
+            "button[data-qa='login_button']").click()
+
         # try:
         # Using implciit wait and explicit wait together causes errors
         #     WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "nowPlayingTopInfo__artContainer__art")))
@@ -46,60 +64,147 @@ class WebScraper:
     def logout(self):
         pandora_logout_url = self.config['URLs']['pandora']['logout']
         self.driver.get(pandora_logout_url)
-    
+
     def close_browser(self):
         self.driver.quit()
-    
-    def get_stations_URL(self):
+
+    def get_stations_URLs(self):
         # Click My Collections
-        self.driver.find_element(By.CSS_SELECTOR, "a[data-qa='header_my_stations_link']").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "a[data-qa='header_my_stations_link']").click()
 
-        self.scroll_to_bottom()
+        self.scroll_page()
 
-        station_elements = self.driver.find_elements(By.CLASS_NAME, "GridItem__caption__link")
-        
-        stations_URL = [element.get_attribute("href") for element in station_elements]
-        
+        station_elements = self.driver.find_elements(
+            By.CLASS_NAME, "GridItem__caption__link")
+        stations_URL = [element.get_attribute(
+            "href") for element in station_elements]
+
         # for element in station_elements:
         #     # (stations +=) adds the letters instead of full string
         #     stations.append(self.driver.find_element(By.CLASS_NAME, "GridItem__caption__link").get_attribute('href'))
         print(len(stations_URL))
         print(stations_URL)
-        
+
         return stations_URL
-        
+
     def get_songs(self, stations_URL):
-        thumbs_ups = []
-        thumbs_downs = []
         data = {'stations': []}
+        errors = []
 
         for station_URL in stations_URL:
-            self.go_to_URL(station_URL)
-            name = self.driver.find_element(By.CLASS_NAME, "EditableTitle__input").get_attribute("value")
-            print(name)
-            break
-            # data['stations'].append({
-            #     'name':,
-            #     'URL': station_URL,
-            #     'thumbs_ups':,
-            #     'thumbs_downs':
-            # })
+            try:
+                self.go_to_URL(station_URL)
+                VIEW_PAUSE_TIME = random.uniform(1, 2)
+                time.sleep(VIEW_PAUSE_TIME)
 
-    def scroll_to_bottom(self):
+                temp = self.driver.find_elements(
+                    By.CLASS_NAME, "DisplayThumb__flexWrap__count")
+                thumbs_up_count, thumbs_down_count = int(
+                    temp[0].text), int(temp[1].text)
+
+                if thumbs_up_count == 0 and thumbs_down_count == 0:
+                    continue
+                up_songs, up_artists, down_songs, down_artists = [], [], [], []
+
+                try:
+                    name = self.driver.find_element(
+                        By.CLASS_NAME, "EditableTitle__input").get_attribute("value")
+                except:
+                    name = self.driver.find_element(
+                        By.CLASS_NAME, "StationDetailsHeader__title__mainTitle__text").text
+                print(
+                    f"{name}\n\tLikes: {thumbs_up_count} Dislike: {thumbs_down_count}")
+
+                if thumbs_up_count > 0:
+                    feedback_list = self.driver.find_element(
+                        By.CLASS_NAME, "FeedbackList__list")
+                    self.scroll_element(feedback_list)
+                    up_songs, up_artists = self.collect_feedback_list(
+                        feedback_list)
+
+                if thumbs_down_count > 0:
+                    down_button = self.driver.find_element(By.CLASS_NAME, "FeedbackList__ListHeader").find_element(
+                        By.CLASS_NAME, "DisplayThumb--down")
+                    self.scroll_element_into_middle(down_button)
+                    down_button.click()
+
+                    feedback_list = self.driver.find_element(
+                        By.CLASS_NAME, "FeedbackList__list")
+
+                    self.scroll_element(feedback_list)
+                    down_songs, down_artists = self.collect_feedback_list(
+                        feedback_list)
+                print(
+                    f"\tActual Likes: {len(up_songs)} Actual Dislike: {len(down_songs)}")
+                data['stations'].append({
+                    'name': name,
+                    'URL': station_URL,
+                    'songs': {'name': up_songs + down_songs, 'artist': up_artists + down_artists, 'liked': ([True]*len(up_songs)) + [False]*len(down_songs)}
+                })
+            except KeyboardInterrupt:
+                print(sys.exc_info())
+                break
+            except:
+                exception = str(sys.exc_info())
+                errors.append({station_URL: exception})
+                continue
+
+        with open('data.json', 'w', encoding='utf-8') as f1, open('errors.json', 'w', encoding='utf-8') as f2:
+            json.dump(data, f1, ensure_ascii=False, indent=1)
+            json.dump(errors, f2, ensure_ascii=False, indent=1)
+
+    def collect_feedback_list(self, feedback_list):
+        time.sleep(random.uniform(0.75, 1.5))
+
+        songs = feedback_list.find_elements(
+            By.CLASS_NAME, "RowItemCenterColumn__mainText")
+        artists = feedback_list.find_elements(
+            By.CLASS_NAME, "RowItemCenterColumn__secondText")
+
+        songs = [element.text for element in songs]
+        artists = [element.text for element in artists]
+        return songs, artists
+
+    def scroll_element_into_middle(self, element):
+        time.sleep(random.uniform(0.75, 1.5))
+
+        script = "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);" + \
+            "var elementTop = arguments[0].getBoundingClientRect().top;" + \
+            "window.scrollBy(0, elementTop-(viewPortHeight/2));"
+        self.driver.execute_script(script, element)
+
+    def scroll_element(self, element):
+        self.scroll_element_into_middle(element)
+        element.click()
         while True:
             SCROLL_PAUSE_TIME = random.uniform(0.75, 1.5)
-            last_height = self.driver.execute_script("return document.body.scrollHeight")
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            new_height = element.size['height']
+            webdriver.ActionChains(self.driver).move_to_element(element).send_keys(
+                Keys.END).send_keys(Keys.ARROW_UP).send_keys(Keys.END).perform()
             time.sleep(SCROLL_PAUSE_TIME)
-            self.driver.execute_script("window.scroll(0, -30);")
+            last_height = element.size['height']
+            # print(f"{element} --- {last_height} --- {new_height}")
+            if last_height == new_height:
+                time.sleep(SCROLL_PAUSE_TIME)
+                return
+
+    def scroll_page(self):
+        while True:
+            SCROLL_PAUSE_TIME = random.uniform(0.75, 1.5)
+
+            scroll_height = "return document.body.scrollHeight;"
+            scroll_down = "window.scrollTo(0, document.body.scrollHeight);"
+
+            last_height = self.driver.execute_script(scroll_height)
+            self.driver.execute_script(scroll_down)
             time.sleep(SCROLL_PAUSE_TIME)
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            self.driver.execute_script("window.scroll(0, -10);")
+            time.sleep(SCROLL_PAUSE_TIME)
+            self.driver.execute_script(scroll_down)
+            new_height = self.driver.execute_script(scroll_height)
             if last_height == new_height:
                 return
 
-
     def go_to_URL(self, URL):
         self.driver.get(URL)
-
-    
