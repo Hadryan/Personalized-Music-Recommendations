5c1fa3797799af8bdf835e2988eda8f0097bf290
vphan404 <vphan404@users.noreply.github.com>
2020-09-02 12:23:21 -0400

    Created activate_webscraper to create and use webscraper

--- main.py
+++ main.py
@@ -2,37 +2,42 @@
 import time
 
 # Related third party
-from bs4 import BeautifulSoup # html/xml parser
-import pandas as pd # dataframe
-import requests # for making HTTP requests
+from bs4 import BeautifulSoup  # html/xml parser
+import pandas as pd  # dataframe
+import requests  # for making HTTP requests
 
 # Local application
-import webscraper
 from webscraper import WebScraper
 
-### Web scrape songs from Pandora with liked or not liked attribute
+# Web scrape songs from Pandora with liked or not liked attribute
 
-### Create dataframe of Pandora songs with Spotify audio features
 
-### Create model based on features
+def activate_webscraper():
+    webscraper = WebScraper(is_headless=True)
+    webscraper.login()
+    station_URLs = webscraper.get_stations_URLs()
+    webscraper.get_songs(station_URLs)
+    # webscraper.get_songs(["https://www.pandora.com/station/34242"])
+    # webscraper.get_songs(["https://www.pandora.com/station/34242",
+    #                       "https://www.pandora.com/station/4550091192589184342", "https://www.pandora.com/station/3852655062826970454"])
+    input('Press ENTER to exit browser')
+    webscraper.close_browser()
 
-### Adjust models via balancing and different estimators
+# Create dataframe of Pandora songs with Spotify audio features
 
-### Find new recommendations
-## Randomly select songs from the Spotify database
-## Use graph network to find songs of similiar users
-## Use songs similiar in features
+# Create model based on features
+
+# Adjust models via balancing and different estimators
+
+# Find new recommendations
+# Randomly select songs from the Spotify database
+# Use graph network to find songs of similiar users
+# Use songs similiar in features
+
 
 def main():
-  webscraper = WebScraper(is_headless=False)
-  webscraper.login()
-  time.sleep(6)
-  webscraper.get_stations()
-  input('Press ENTER to exit browser')
-  webscraper.logout()
-  time.sleep(2)
-  webscraper.close_browser()
-  input('Press ENTER to exit program')
+    activate_webscraper()
+
 
 if __name__ == "__main__":
     main()
