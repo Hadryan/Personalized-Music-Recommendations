16bfb76bdd63db89267a6cbbd0e747024455cf1e
vphan404 <vphan404@users.noreply.github.com>
2020-09-04 12:53:22 -0400

    Finished converting data to dataframe

--- data_engineering.py
+++ data_engineering.py
@@ -5,7 +5,20 @@ import requests  # for making HTTP requests
 
 class PandoraSongSet:
     def __init__(self, data_file="data.json"):
-        with open(data_file, 'r', encoding="utf8") as f:
-            self.data = json.load(f)
+        try:
+            with open(data_file, 'r', encoding="utf8") as f:
+                self.data = json.load(f)
+        except EnvironmentError as IO_error:
+            print("Something is wrong with the file")
+        self.songs = pd.DataFrame(columns=[
+            'name', 'artist', 'liked'])
 
-    def create_master_list(self):
+    def create_master_dataframe(self):
+        for station in self.data['stations']:
+            name = station['songs']['name']
+            artist = station['songs']['artist']
+            liked = station['songs']['liked']
+            station_songs = list(zip(name, artist, liked))
+
+            self.songs = self.songs.append(
+                pd.DataFrame(station['songs']))
