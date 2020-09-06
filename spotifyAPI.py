d377a06795bcadb0b2abebf947db1c4bd14b628c
vphan404 <vphan404@users.noreply.github.com>
2020-09-06 13:33:02 -0400

    Created functions to check if track exists and find their id

--- spotifyAPI.py
+++ spotifyAPI.py
@@ -8,7 +8,7 @@ class SpotifyAPI:
     def __init__(self):
         self.authenticate()
 
-    def authenticate(self, credentials_file="credentias.json"):
+    def authenticate(self, credentials_file="credentials.json"):
         try:
             with open(credentials_file, 'r', encoding="utf8") as f:
                 credentials = json.load(f)
@@ -17,8 +17,22 @@ class SpotifyAPI:
 
             client_credentials_manager = SpotifyClientCredentials(
                 client_id, client_secret)
-            sp = spotipy.Spotify(
+            self.sp = spotipy.Spotify(
                 client_credentials_manager=client_credentials_manager)
             print("Spotify authentication successful")
         except EnvironmentError as io_error:
             print(io_error)
+
+    def track_exists(self, track):
+        track_json = self.sp.search(track, limit=1)
+        test = track_json['tracks']['total']
+        return True if test > 0 else False
+
+    def get_track_id(self, track):
+        try:
+            track_json = self.sp.search(track, limit=1)
+            track_id = track_json['tracks']['items'][0]['id']
+            return track_id
+        except Exception as e:
+            print(e)
+            return "NaN"
